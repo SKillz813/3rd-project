11127d59e531cd09ca2e82437223c26ff9591b3b
stauros5 <sbullntolas@gmail.com>
2025-01-17 17:36:57 +0200

    generate_report.c

--- generate_report.c
+++ generate_report.c
@@ -2,7 +2,7 @@
 
 // Συνάρτηση για τη δημιουργία αναφοράς πωλήσεων
 void generate_report(Product catalog[]) {
-    printf("\n--- Αναφορά Πωλήσεων ---\n");
+    printf("Αναφορά Πωλήσεων");
     for (int i = 0; i < MAX_PRODUCTS; i++) {
         printf("%s: %d τεμάχια διαθέσιμα\n", catalog[i].description, catalog[i].item_count);
     }
