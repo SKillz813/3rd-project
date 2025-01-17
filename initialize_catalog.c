9adc9eca102b6cdfb05704c9f16e00ad674b2e30
stauros5 <sbullntolas@gmail.com>
2025-01-17 17:37:58 +0200

    initialize_catalog.c

--- initialize_catalog.c
+++ initialize_catalog.c
@@ -4,7 +4,7 @@
 void initialize_catalog(Product catalog[]) {
     for (int i = 0; i < MAX_PRODUCTS; i++) {
         snprintf(catalog[i].description, sizeof(catalog[i].description), "Προϊόν %d", i + 1);
-        catalog[i].price = (float)(10 + i); // Αυθαίρετη τιμή
+        catalog[i].price = (float)(10 + i); // Τυχαία τιμή
         catalog[i].item_count = 2;          // Αρχικός αριθμός τεμαχίων
     }
 }
