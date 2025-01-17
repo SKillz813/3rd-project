73c5eec3f2d3566b8ac9ebd55ab5368c76a25523
stauros5 <sbullntolas@gmail.com>
2025-01-17 17:01:08 +0200

    initialize_catalog.c

---
+++ initialize_catalog.c
@@ -0,0 +1,10 @@
+#include "Functions.h"
+
+// Συνάρτηση για την αρχικοποίηση του καταλόγου προϊόντων
+void initialize_catalog(Product catalog[]) {
+    for (int i = 0; i < MAX_PRODUCTS; i++) {
+        snprintf(catalog[i].description, sizeof(catalog[i].description), "Προϊόν %d", i + 1);
+        catalog[i].price = (float)(10 + i); // Αυθαίρετη τιμή
+        catalog[i].item_count = 2;          // Αρχικός αριθμός τεμαχίων
+    }
+}
